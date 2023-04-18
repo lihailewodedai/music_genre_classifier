005106fc2dac422816eb3c589ffc5ee121ead173
lihailewodedai <zggzmyt@gmail.com>
2023-04-18 19:30:55 +0100

    react go button trigger timer

---
+++ MusicClassifier/public/headerTitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function headerTitle() {
+    return (
+        <>
+          <h1>Hello, welcome to...</h1>
+          <h2>Learn React!</h2>
+        </>
+      );
+}
+
--- MusicClassifier/public/src/reactComponents/headerTitle.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function headerTitleComponent() {
-    return <span> Sound Recognizer </span>;
-}
-
-export default headerTitleComponent
